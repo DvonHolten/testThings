f72050ad2af3c2551c16f72a219227cb003296f3
Dieter von Holten <info@dvholten.de>
2023-08-11 14:41:49 +0200

    the first batch of files for Diffblue tests

---
+++ src/main/java/com/dvh/diffblue1/SalesProduct.java
@@ -0,0 +1,15 @@
+package com.dvh.diffblue1;
+
+public class SalesProduct
+        implements ISalesProduct {
+
+    int rowId = -1;
+
+    String productCode;
+
+    String productName;
+
+    String unStoffNr;
+
+    RecordStatus recStatus;
+}
---
+++ src/test/java/com/dvh/diffblue1/Diffblue1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvh.diffblue1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diffblue1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
